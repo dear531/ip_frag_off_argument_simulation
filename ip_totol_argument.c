777648d5234ebfc8d416547c1cf52504453a8baa
zly <dear531@21cn.com>
2013-08-28 11:10:05 +0800

    ip tot_len argument simulation displacement

---
+++ ip_totol_argument.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	short tot_len	= 0xffff;
+	short tmp	= 0;
+	tmp = tot_len;
+	printf("old tmp:%0x\n", tmp);
+	tmp = tot_len & ~(0x7 << 13);
+	printf("0x7 << 13 tmp:%0x\n", tmp);
+	tmp = tot_len & ~(0x3 << 14);
+	printf("0x3 << 14 tmp:%0x\n", tmp);
+	tmp = tot_len & ~(0x1 << 15);
+	printf("0x1 << 15 tmp:%0x\n", tmp);
+	return 0;
+}
