571619cffa7693bfaa56e2596ca2ede88a5eeb70
zly <dear531@21cn.com>
2013-08-28 13:36:11 +0800

    rename tot_len to frag_off

--- ip_frag_off_argument.c
+++ ip_frag_off_argument.c
@@ -2,15 +2,15 @@
 
 int main(int argc, char *argv[])
 {
-	short tot_len	= 0xffff;
+	short frag_off	= 0xffff;
 	short tmp	= 0;
-	tmp = tot_len;
+	tmp = frag_off;
 	printf("old tmp:%04x\n", (unsigned short)tmp);
-	tmp = tot_len & ~(0x7 << 13);
+	tmp = frag_off & ~(0x7 << 13);
 	printf("0x7 << 13 tmp:%0x\n", tmp);
-	tmp = tot_len & ~(0x3 << 14);
+	tmp = frag_off & ~(0x3 << 14);
 	printf("0x3 << 14 tmp:%0x\n", tmp);
-	tmp = tot_len & ~(0x1 << 15);
+	tmp = frag_off & ~(0x1 << 15);
 	printf("0x1 << 15 tmp:%0x\n", tmp);
 	return 0;
 }
