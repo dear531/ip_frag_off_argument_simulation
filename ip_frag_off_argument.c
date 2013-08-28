ecb4bcc3e3f44941738d46f03661724b2ffe78e3
zly <dear531@21cn.com>
2013-08-28 14:27:43 +0800

    addition IP flags show

--- ip_frag_off_argument.c
+++ ip_frag_off_argument.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/* /usr/src/linux-source-3.5.0/linux-source-3.5.0/include/net/ip.h */
+/* IP flags. */
+#define IP_CE		0x8000		/* Flag: "Congestion"		*/
+#define IP_DF		0x4000		/* Flag: "Don't Fragment"	*/
+#define IP_MF		0x2000		/* Flag: "More Fragments"	*/
+#define IP_OFFSET	0x1FFF		/* "Fragment Offset" part	*/
 
 int main(int argc, char *argv[])
 {
@@ -12,5 +18,8 @@ int main(int argc, char *argv[])
 	printf("0x3 << 14 tmp:%0x\n", tmp);
 	tmp = frag_off & ~(0x1 << 15);
 	printf("0x1 << 15 tmp:%0x\n", tmp);
+	printf("IP_OFFSET:%04x\n", IP_OFFSET);
+	printf("~(0x7 << 13):%04x\n", (unsigned short)~(0x7 << 13));
+
 	return 0;
 }
