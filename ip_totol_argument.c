3b42351b744b97f5dc62a88e4256f9e79d4f1f5f
zly <dear531@21cn.com>
2013-08-28 12:06:42 +0800

    fix bug printf short tmp show 4 byte ffffffff

--- ip_totol_argument.c
+++ ip_totol_argument.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 	short tot_len	= 0xffff;
 	short tmp	= 0;
 	tmp = tot_len;
-	printf("old tmp:%0x\n", tmp);
+	printf("old tmp:%04x\n", (unsigned short)tmp);
 	tmp = tot_len & ~(0x7 << 13);
 	printf("0x7 << 13 tmp:%0x\n", tmp);
 	tmp = tot_len & ~(0x3 << 14);
